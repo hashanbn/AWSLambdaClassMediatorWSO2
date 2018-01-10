6ffa6e64d3fb0e87d332b13edab7e7959c9e15c4
Hashan <hashanbn@gmail.com>
2018-01-10 15:36:22 +0530

    Meta data changes

--- src/main/java/org/wso2/awsmediator/AWSLambdaMediator.java
+++ src/main/java/org/wso2/awsmediator/AWSLambdaMediator.java
@@ -9,62 +9,46 @@ import com.amazonaws.services.lambda.AWSLambdaClient;
 import com.amazonaws.services.lambda.model.InvocationType;
 import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.amazonaws.services.lambda.model.InvokeResult;
-import org.apache.axiom.soap.SOAPBody;
-import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.commons.json.JsonUtil;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.OMAbstractFactory;
-import org.apache.axiom.om.OMFactory;
-import org.apache.axiom.soap.SOAPFactory;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import org.apache.synapse.commons.json.JsonUtil;
 import org.json.JSONObject;
 import org.json.XML;
 
-import javax.xml.namespace.QName;
 import java.nio.charset.Charset;
 
-public class AWSClassMediator  extends AbstractMediator{
-    //private static final Log log = LogFactory.getLog(AWSClassMediator.class);
+public class AWSLambdaMediator extends AbstractMediator{
+    //private static final Log log = LogFactory.getLog(AWSLambdaMediator.class);
 
     private String region = "";
     private String functionName = "";
     private String accessKey = "";
     private String secretKey = "";
+    private int maxConnections = 1100;
 
-    public AWSClassMediator () {}
+    public AWSLambdaMediator() {}
 
     public boolean mediate (MessageContext mc) {
         JSONObject jsonBody = XML.toJSONObject(mc.getEnvelope().getBody().toString()).getJSONObject("soapenv:Body").getJSONObject("jsonObject");
 
-        //AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAIB72CLU3VTK5GWWA", "yPQky+ZbGOdWpa+ofbo3AUcnzLtaH00kROfgZrnT");
         AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         ClientConfiguration clientConfiguration = new ClientConfiguration();
 
-        clientConfiguration.setMaxConnections(1100);
+        clientConfiguration.setMaxConnections(maxConnections);
         AWSLambdaClient awsLambdaClient = new AWSLambdaClient(awsCredentials, clientConfiguration);
 
-        //awsLambdaClient.setRegion(Region.getRegion(Regions.US_EAST_2));
-        //awsLambdaClient.setRegion(Region.getRegion(Regions.fromName("us-east-2")));
         awsLambdaClient.setRegion(Region.getRegion(Regions.fromName(region)));
         InvokeRequest invokeRequest = new InvokeRequest();
-        //invokeRequest.setFunctionName("testFunc");
+
         invokeRequest.setFunctionName(functionName);
         invokeRequest.setInvocationType(InvocationType.RequestResponse);
-        //invokeRequest.setPayload("{\"name\":\"Hashan\"}");
+
         invokeRequest.setPayload(jsonBody.toString());
 
         InvokeResult invokeResult = awsLambdaClient.invoke(invokeRequest);
         String s = new String(invokeResult.getPayload().array(), Charset.forName("UTF-8"));
 
-        //String jsonPayload = JsonUtil.jsonPayloadToString(((Axis2MessageContext) mc).getAxis2MessageContext());
-        //System.out.print("Test JSON: ");
-        //System.out.println(jsonPayload);
-        //JSONObject jsonBody = new JSONObject(jsonPayload);
         jsonBody.put("response", s.replaceAll("^\"|\"$", ""));
         String updatedPayload = jsonBody.toString();
 
@@ -85,5 +69,43 @@ public class AWSClassMediator  extends AbstractMediator{
         return 0;
     }
 
+    public void setRegion(String pRegion) {
+        region = pRegion;
+    }
 
+    public void setFunctionName(String pFunctionName) {
+        functionName = pFunctionName;
+    }
+
+    public void setAccessKey(String pAccessKey) {
+        accessKey = pAccessKey;
+    }
+
+    public void setSecretKey(String pSecretKey) {
+        secretKey = pSecretKey;
+    }
+
+    public void setMaxConnections(String pMaxConnections) {
+        maxConnections = Integer.parseInt(pMaxConnections);
+    }
+
+    public String getRegion () {
+        return region;
+    }
+
+    public String getFunctionName () {
+        return functionName;
+    }
+
+    public String getAccessKey () {
+        return accessKey;
+    }
+
+    public String getSecretKey () {
+        return secretKey;
+    }
+
+    public int getMaxConnections() {
+        return maxConnections;
+    }
 }
